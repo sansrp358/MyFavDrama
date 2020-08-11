00e6c75343044866e51fb047a98416381625e7b8
sansrp358 <sanskrutirpaitwar@gmail.com>
2020-08-12 00:18:05 +0530

    my first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,24 @@
+const Sdata = [
+    {
+        sname:"Put your head on my shoulder",
+        imgsrc:"https://upload.wikimedia.org/wikipedia/en/5/57/Put_Your_Head_on_My_Shoulder.jpg",
+        link:"https://youtu.be/FJNqLXmtGKU",
+        title:"A WeTv Original Series"
+
+    },
+    {
+        sname:"Crash landing on you" ,
+        imgsrc:"https://xdigitalnews.com/wp-content/uploads/2020/05/Crash-Landing-On-You-becomes-2nd-cable-drama-to-hit-20-TV-rating.png",
+        link:"https://youtu.be/Q-rGIR3DdNk",
+        title:"A Netflix Original Series"
+
+    },
+    {
+        sname:"Her private life",
+        imgsrc:"https://upload.wikimedia.org/wikipedia/en/thumb/a/a2/Her_Private_Life.jpeg/250px-Her_Private_Life.jpeg",
+        link:"https://youtu.be/kx-B1bBi8So",
+        title:"A WeTv Original Series"
+
+    }
+];
+export default Sdata;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Card from  './Cards';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Sdata from './Sdata';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+<>
+<h1 className='heading_style'>My most favorite K-drama</h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+{Sdata.map((val)=>{
+      return(
+<Card imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}  
+      link={val.link}
+/>
+);
+})}
+
+</>
+,document.getElementById('root')
+);

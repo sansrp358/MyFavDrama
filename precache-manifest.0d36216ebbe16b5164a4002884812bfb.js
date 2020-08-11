1e6749048f75ef631727bd6fee1ab2a5dec7d15f
sansrp358 <sanskrutirpaitwar@gmail.com>
2020-08-12 00:47:31 +0530

    Updates

---
+++ precache-manifest.0d36216ebbe16b5164a4002884812bfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86640df39bc7dc0f3efd4a2e19a802a",
+    "url": "/MyFavDrama/index.html"
+  },
+  {
+    "revision": "9964832e513da36cc7cd",
+    "url": "/MyFavDrama/static/css/main.82a8d1c7.chunk.css"
+  },
+  {
+    "revision": "8097ad6e4c0dac3ee990",
+    "url": "/MyFavDrama/static/js/2.d6388d0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyFavDrama/static/js/2.d6388d0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9964832e513da36cc7cd",
+    "url": "/MyFavDrama/static/js/main.e515d00a.chunk.js"
+  },
+  {
+    "revision": "570e9629fe48131e1484",
+    "url": "/MyFavDrama/static/js/runtime-main.50ff4633.js"
+  }
+]);

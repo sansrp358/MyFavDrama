7488366a847827c142de8d66bb4928c91ed3aeee
sansrp358 <sanskrutirpaitwar@gmail.com>
2020-08-27 23:54:36 +0530

    committed

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from  './Cards';
+import App from './App';
 import './index.css';
-import Sdata from './Sdata';
 
+ReactDOM.render(<App />,document.getElementById('root'));
 
-ReactDOM.render(
-<>
-<h1 className='heading_style'>My most favorite K-drama</h1>
-
-{Sdata.map((val)=>{
-      return(
-<Card imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}  
-      link={val.link}
-/>
-);
-})}
-
-</>
-,document.getElementById('root')
-);
